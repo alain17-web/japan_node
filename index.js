2c887a64a41949a884f58c2940d41e97dd1666a2
alain17-web <web2020.alain@gmail.com>
2024-02-29 22:07:41 +0100

    it works

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const url = require('url')
 const ejs = require('ejs')
 const path = require('path')
 const fs = require('fs')
-const queryString = require('querystring')
+
 
 
 
@@ -15,12 +15,14 @@ const server = http.createServer((req, res) => {
 
     res.setHeader('Content-Type', 'text/html')
 
+    //CSS
+
     if (pathname.startsWith('/styles/')) {
         const cssPath = path.join(__dirname, 'views', pathname)
         fs.readFile(cssPath, (err, data) => {
             if (err) {
                 res.statusCode = 404
-                res.end('Erreur : file not found')
+                res.end('Error : file not found')
             } else {
                 res.setHeader('Content-Type', 'text/css')
                 res.end(data)
@@ -29,85 +31,130 @@ const server = http.createServer((req, res) => {
         return
     }
 
-    let jsonFilePath
+    //IMG
 
-    switch (pathname) {
-        case '/':
-            ejs.renderFile('views/home.ejs', {}, {}, (err, str) => {
-                if (err) {
-                    console.log(err)
-                }
-                res.end(str)
-            })
-            break;
-
-        case '/destinations':
-
-            jsonFilePath = path.join(__dirname, 'japon.json')
-
-            fs.readFile(jsonFilePath, (err, data) => {
-                if (err) {
-                    console.log(err)
-
-                }
-
-                const destinationsData = JSON.parse(data)
-
-                ejs.renderFile('views/destinations.ejs', { destinationsData: destinationsData }, {}, {}, (err, str) => {
-                    if (err) {
-                        console.log(err)
-                    }
-                    res.end(str)
-                })
-            })
-            break;
-
-        case '/singleDestination':
-            const destinationId = parseInt(url.parse(req.url,true).query.id)
-            jsonFilePath = path.join(__dirname, 'japon.json')
-
-            fs.readFile(jsonFilePath, (err, data) => {
-                if(err){
-                    console.log(err)
-                }
-
-                const destinationsDataSingle = JSON.parse(data)
-                const singleDestinationData = destinationsDataSingle.find(destination => destination.id === destinationId)
-
-                ejs.renderFile('views/singleDestination.ejs', { singleDestinationData: singleDestinationData }, {}, {}, (err, str) => {
-                    if (err) {
-                        console.log(err)
-                    }
-                    res.end(str)
-                })
-            })
-            
-            break;
-
-
-        case '/contacts':
-            ejs.renderFile('views/contact.ejs', {}, {}, (err, str) => {
-                if (err) {
-                    console.log(err)
-                }
-                res.end(str)
-            })
-            break;
-
-        case '/login':
-            ejs.renderFile('views/login.ejs', {}, {}, (err, str) => {
-                if (err) {
-                    console.log(err)
-                }
-                res.end(str)
-            })
-            break;
-
-        default:
-            res.statusCode = 404;
-            res.end('Page not found')
+    if (pathname.startsWith('/img/')) {
+        const imgPath = path.join(__dirname, 'public', pathname)
+        fs.readFile(imgPath, (err, data) => {
+            if (err) {
+                console.log(err)
+                res.statusCode = 404
+                res.end('Error : image not found')
+            }
+            else {
+                const ext = path.extname(pathname).slice(1);
+                const contentType = `image/${ext}`;
+                res.setHeader('Content-Type', contentType);
+                res.end(data);
+            }
+        })
+        return
     }
-})
+
+    let jsonFilePath
+    // let activePage
+    //ACTIVE 
+    // switch (pathname) {
+    //     case '/':
+    //         activePage = 'home';
+    //         break;
+    //     case '/destinations':
+    //         activePage = 'destinations';
+    //         break;
+    //     case '/contacts':
+    //         activePage = 'contacts';
+    //         break;
+    //     case '/login':
+    //         activePage = 'login';
+    //         break;
+    //     default:
+    //         return
+    // }
+
+    // ejs.renderFile('views/header.ejs', { activePage: activePage }, {}, (err, headerStr) => {
+    //     if (err) {
+    //         console.log(err);
+    //     }
+
+        //ROUTES
+
+        switch (pathname) {
+            case '/':
+                ejs.renderFile('views/home.ejs', {}, {}, (err, str) => {
+                    if (err) {
+                        console.log(err);
+                    }
+                    res.end(str);
+                });
+                break;
+
+            case '/destinations':
+
+                jsonFilePath = path.join(__dirname, 'japon.json')
+
+                fs.readFile(jsonFilePath, (err, data) => {
+                    if (err) {
+                        console.log(err)
+
+                    }
+
+                    const destinationsData = JSON.parse(data)
+
+                    ejs.renderFile('views/destinations.ejs', { destinationsData: destinationsData },{}, {}, (err, str) => {
+                        if (err) {
+                            console.log(err)
+                        }
+                        res.end(str)
+                    })
+
+                })
+                break;
+
+            case '/singleDestination':
+                const destinationId = parseInt(url.parse(req.url, true).query.id)
+                jsonFilePath = path.join(__dirname, 'japon.json')
+
+                fs.readFile(jsonFilePath, (err, data) => {
+                    if (err) {
+                        console.log(err)
+                    }
+
+                    const destinationsDataSingle = JSON.parse(data)
+                    const singleDestinationData = destinationsDataSingle.find(destination => destination.id === destinationId)
+
+                    ejs.renderFile('views/singleDestination.ejs', { singleDestinationData: singleDestinationData }, {}, {}, (err, str) => {
+                        if (err) {
+                            console.log(err)
+                        }
+                        res.end(str)
+                    })
+                })
+                break;
+
+            case '/contacts':
+                ejs.renderFile('views/contact.ejs', {}, {}, (err, str) => {
+                    if (err) {
+                        console.log(err);
+                    }
+                    res.end(str);
+                });
+                break;
+
+            case '/login':
+                ejs.renderFile('views/login.ejs', {}, {}, (err, str) => {
+                    if (err) {
+                        console.log(err);
+                    }
+                    res.end(str);
+                });
+                break;
+
+            default:
+                res.statusCode = 404;
+                res.end('Page not found')
+        }
+    })
+// })
 
 server.listen(8080, () => {
     console.log('server is running on 8080')
